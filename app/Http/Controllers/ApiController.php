80a8b55754585fea62c6b7a8855da73667d00c92
esuEichi <aloneware94@gmail.com>
2017-11-13 19:23:07 +0900

    change api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+require_once __DIR__ . '/vendor/autoload.php';
+
+class ApiController extends Controller
+{
+    //
+    function index(){
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+        $sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+        $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
+
+        foreach ($events as $event) {
+            if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
+            !($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+            continue;
+        }
+        $bot->replyText($event->getReplyToken(), $event->getText());
+        }
+    }
+}
