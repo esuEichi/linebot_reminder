dcd7c8ff31e1b009cc5d9d437caa64626d0266a4
esuEichi <aloneware94@gmail.com>
2017-11-21 17:29:50 +0900

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,19 +13,8 @@ class ApiController extends Controller
     function index(Request $request){
         $httpClient = new CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
         $bot = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
-
-        \Log::debug($request);
-
-        //$events = file_get_contents('php://input');
-        //$replyToken = explode('"',explode('replyToken":"', $events)[1])[0];
-
+        \Log::debug(env('CHANNEL_SECRET'));
         $replyToken = $request['events'][0]['replyToken'];
-
-//        \Log::debug("access: " + env('CHANNEL_ACCESS_TOKEN'));
-//        \Log::debug("secret: " + env('CHANNEL_SECRET'));
-
-//        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("hogehoge");        
-        //$bot->replyText($events->event[0]['replyToken'], $textMessageBuilder);
         $bot->replyText($replyToken, "hogehoge");
 
     }
