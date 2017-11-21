35649b446b083b158ebbc99932f4670c9a08b17d
esuEichi <aloneware94@gmail.com>
2017-11-21 16:55:39 +0900

    test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,7 +16,7 @@ class ApiController extends Controller
         //$sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
         \Log::debug(file_get_contents('php://input'));
 
-        $events = json_decode(file_get_contents('php://input'));
+        $events = json_decode(file_get_contents('php://input'), true);
         /*
         foreach ($events as $event) {
             if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
@@ -31,7 +31,7 @@ class ApiController extends Controller
         //$json = mb_convert_encoding($events, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
         //\Log::debug(var_dump($json));
 
-        \Log::debug(var_dump($events->replyToken));
+        \Log::debug(var_dump($events['replyToken']));
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("hogehoge");        
         //$bot->replyText($events->event[0]['replyToken'], $textMessageBuilder);
         $bot->replyText($events->event[0]['replyToken'], "hogehoge");
