8423482f81edb9778427152211620916cb5a78b4
esuEichi <aloneware94@gmail.com>
2017-11-24 10:09:08 +0900

    asdf

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,15 +11,15 @@ class ApiController extends Controller
 {
     //
     function index(Request $request){
-        $httpClient = new CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
-        $bot = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
-        //\Log::debug("SECRET: ".var_dump(env('CHANNEL_SECRET')));
-        //\Log::debug($replyToken = $request['events'][0]['replyToken']);
+        $httpClient = new CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+        $bot = new LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+        \Log::debug("SECRET: ".var_dump(getenv('CHANNEL_SECRET')));
+        \Log::debug($replyToken = $request['events'][0]['replyToken']);
         
         $replyToken = $request['events'][0]['replyToken'];
         $temp = $bot->replyText($replyToken, "hogehoge");
 
-        //\Log::debug($temp);
+        \Log::debug(var_dump($temp));
 
     }
 }
