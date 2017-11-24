9ff125d6cdaa3adcb4001722de8d8adfdc17bfa2
esuEichi <aloneware94@gmail.com>
2017-11-24 10:04:10 +0900

    fa

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -13,7 +13,7 @@ class ApiController extends Controller
     function index(Request $request){
         $httpClient = new CurlHTTPClient(env('CHANNEL_ACCESS_TOKEN'));
         $bot = new LINEBot($httpClient, ['channelSecret' => env('CHANNEL_SECRET')]);
-        \Log::debug("SECRET: " + var_dump(env('CHANNEL_SECRET')));
+        \Log::debug("SECRET: ".var_dump(env('CHANNEL_SECRET')));
         \Log::debug($replyToken = $request['events'][0]['replyToken']);
         
         $replyToken = $request['events'][0]['replyToken'];
