d419f33122a66c0e6ff4b3ed857cb02c468f5439
esuEichi <aloneware94@gmail.com>
2017-12-01 10:27:56 +0900

    gasg

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 //require_once __DIR__ . '/vendor/autoload.php';
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot\MassageBuilder;
 use \LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
 use App\remind;
 
 class ApiController extends Controller
